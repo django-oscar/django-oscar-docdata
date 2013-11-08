abf523c1762f69e1edd6c79f9a67c36926c11ec5
Diederik van der Boor <vdboor@edoburu.nl>
2013-11-08 10:29:15 +0100

    Add base package layout

---
+++ README.rst
@@ -0,0 +1,11 @@
+django-oscar-docdata
+====================
+
+Payment gateway integration for `Docdata Payments <http://www.docdatapayments.com/>`_ in django-oscar_.
+DocData Payments is a large payment gateway based in The Netherlands that supports more then 40 international payment methods.
+
+This package uses django-docdata_ internally as backend.
+
+
+.. _django-docdata: https://github.com/dokterbob/django-docdata
+.. _django-oscar: https://github.com/tangentlabs/django-oscar
---
+++ setup.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+from os import path
+import codecs
+import os
+import re
+import sys
+
+
+# When creating the sdist, make sure the django.mo file also exists:
+if 'sdist' in sys.argv:
+    try:
+        os.chdir('oscar_docdata')
+        from django.core.management.commands.compilemessages import compile_messages
+        compile_messages(sys.stderr)
+    finally:
+        os.chdir('..')
+
+
+def read(*parts):
+    file_path = path.join(path.dirname(__file__), *parts)
+    return codecs.open(file_path, encoding='utf-8').read()
+
+
+def find_version(*parts):
+    version_file = read(*parts)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
+    if version_match:
+        return str(version_match.group(1))
+    raise RuntimeError("Unable to find version string.")
+
+
+setup(
+    name='django-oscar-docdata',
+    version=find_version('oscar_docdata', '__init__.py'),
+    license='Apache License, Version 2.0',
+
+    install_requires=[
+        'django-docdata>=0.2',
+        'django-oscar>=0.3'  # used from django-oscar-paymentexpress
+    ],
+    requires=[
+        'Django (>=1.4)',
+    ],
+
+    description='Docdata Payments Gateway integration for django-oscar',
+    long_description=read('README.rst'),
+
+    author='Diederik van der Boor',
+    author_email='opensource@edoburu.nl',
+
+    url='https://github.com/edoburu/django-oscar-docdata',
+    download_url='https://github.com/edoburu/django-oscar-docdata/zipball/master',
+
+    packages=find_packages(exclude=('example*',)),
+    include_package_data=True,
+
+    #test_suite = 'runtests',
+
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Office/Business :: Financial',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
