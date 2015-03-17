cd3ff01d4973469ab923021c5d7fc062cbcca2ec
Diederik van der Boor <vdboor@edoburu.nl>
2015-03-17 16:20:22 +0100

    update_docdata_order: add --all option to update all orders

--- oscar_docdata/management/commands/update_docdata_order.py
+++ oscar_docdata/management/commands/update_docdata_order.py
@@ -1,42 +1,66 @@
 import logging
+from optparse import make_option
 from django.core.management.base import BaseCommand, CommandError
 
 from django.db import transaction
+from oscar_docdata.exceptions import DocdataStatusError
 from oscar_docdata.facade import get_facade
 from oscar_docdata.models import DocdataOrder
 
 
 class Command(BaseCommand):
     help = "Update the status of the given orders"
+    option_list = (
+        make_option('--all', action='store_true', dest='all', default=False,
+            help="Update all orders"),
+    ) + BaseCommand.option_list
+
 
     def handle(self, *args, **options):
         """
         Update the status.
         """
         # At -v2 SOAP requests are outputted.
+        do_all = options['all']
         verbosity = int(options['verbosity'])
-        logging.getLogger('suds.transport').setLevel('INFO' if verbosity < 2 else 'DEBUG')
-
-        if not args:
-            raise CommandError("Expected order numbers as argument")
+        logging.getLogger('oscar_docdata.interface').setLevel('WARNING' if verbosity < 2 else 'DEBUG')
+        logging.getLogger('suds.transport').setLevel('INFO' if verbosity < 3 else 'DEBUG')
 
         qs = DocdataOrder.objects.active_merchants()
-
         facade = get_facade()
 
-        for order_number in args:
-            try:
-                order = qs.get(merchant_order_id=order_number)
-            except DocdataOrder.DoesNotExist:
-                self.stderr.write(u"- Order does not exist: {0}\n".format(order_number))
-                continue
+        if do_all:
+            orders = qs.all()
 
-            self.stdout.write(u"- Checking {0}\n".format(order_number))
+            if args:
+                raise CommandError("No order numbers have to be provided for --all")
+        else:
+            if not args:
+                raise CommandError("Expected order numbers as argument")
+
+            # First get all orders, check them.
+            orders = []
+            for order_number in args:
+                try:
+                    order = qs.get(merchant_order_id=order_number)
+                except DocdataOrder.DoesNotExist:
+                    self.stderr.write(u"- Order does not exist: {0}\n".format(order_number))
+                    continue
+                else:
+                    orders.append(order)
+
+        for order in orders:
+            self.stdout.write(u"- Checking {0}\n".format(order.merchant_order_id))
 
             with transaction.atomic():
                 # First request the order at docdata, avoid expiring an order which missed an update (very unlikely)
                 old_status = order.status
-                facade.update_order(order)
+                try:
+                    facade.update_order(order)
+                except DocdataStatusError as e:
+                    self.stderr.write(u"{0}\n".format(e))
+                    continue
+
                 if order.status == old_status:
                     self.stderr.write(u"  Order {0} status unchanged, remained: {1}".format(order.merchant_order_id, order.status))
                 else:
