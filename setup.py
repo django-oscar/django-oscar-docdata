6897d59aea0a44135b4e665fcda91d24cdcc7b3e
Diederik van der Boor <vdboor@edoburu.nl>
2015-12-17 11:40:45 +0100

    Improve compilemessages in setup.py

--- setup.py
+++ setup.py
@@ -9,11 +9,13 @@ import sys
 
 # When creating the sdist, make sure the django.mo file also exists:
 if 'sdist' in sys.argv or 'develop' in sys.argv:
+    os.chdir('oscar_docdata')
     try:
-        os.chdir('oscar_docdata')
-        if os.path.exists('locale'):
-            from django.core.management.commands.compilemessages import compile_messages
-            compile_messages(sys.stderr)
+        from django.core import management
+        management.call_command('compilemessages', stdout=sys.stderr, verbosity=1)
+    except ImportError:
+        if 'sdist' in sys.argv:
+            raise
     finally:
         os.chdir('..')
 
