84ed27996b8555a3a36cf9d44d030f7a4ae2e674
Martijn Jacobs <martijn@devopsconsulting.nl>
2019-01-31 21:58:04 +0100

    Updated the get-wsdl script to get version 1_3 as well
    and remove the empty xsd=2 files

--- docs/get-wsdl.sh
+++ docs/get-wsdl.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-for version in 1_0 1_1 1_2
+for version in 1_0 1_1 1_2 1_3
 do
   curl "https://test.docdatapayments.com/ps/services/paymentservice/$version?wsdl" > wsdl-$version.wsdl
   curl "https://test.docdatapayments.com/ps/services/paymentservice/$version?xsd=1" > xsd1-$version.xsd
-  curl "https://test.docdatapayments.com/ps/services/paymentservice/$version?xsd=2" > xsd2-$version.xsd
 done
