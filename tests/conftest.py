b8f9eb2326c393cae01ad50bb9b844f1137360f2
Martijn Jacobs <martijn@devopsconsulting.nl>
2019-03-20 22:57:45 +0100

    Added a mock_transport fixture which can be used in the tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,8 @@
+import os
+import tempfile
+
 from django.contrib.auth import get_user_model
 from django.core.management import call_command
-
-from oscar.core.loading import get_model
 
 from oscar_docdata.gateway import DocdataAPIVersionPlugin
 
@@ -13,10 +14,6 @@ from .suds_transport import DocdataMockTransport
 
 User = get_user_model()
 
-Country = get_model('address', 'Country')
-UserAddress = get_model('address', 'UserAddress')
-SourceType = get_model('payment', 'SourceType')
-
 pytest_plugins = "tests.fixtures"
 
 
@@ -26,25 +23,16 @@ def django_db_setup(django_db_setup, django_db_blocker):
         # load a country so we can fill out a shipping address
         call_command("loaddata", "sandbox/fixtures/countries.json")
 
-        # loads books so we have products to do a checkout with
-        call_command("oscar_import_catalogue", "sandbox/fixtures/books.csv")
-
-        # add a customer to login with
-        user = User(username="customer", email="customer@oscarcommerce.com")
-        user.set_password("customer")
-        user.save()
-
-        # and a address so we can checkout
-        UserAddress.objects.create(
-            user=user, title="Mr", first_name="John", last_name="Doe", line1="Just a street 1",
-            line4="Amsterdam", postcode="1111AA", country=Country.objects.get(pk="NL"))
-
-        # add a docdata payment sourcetype
-        SourceType.objects.create(name="Docdata Payments", code="docdata")
+        # import one book from the sandbox csv data
+        csv_data = open("sandbox/fixtures/books.csv").readlines()
+        with tempfile.TemporaryDirectory() as tmp_dir:
+            csv_path = os.path.join(tmp_dir, "onebook.csv")
+            open(csv_path, "wb").write(csv_data[-1].encode('utf-8'))
+            call_command("oscar_import_catalogue", csv_path)
 
 
 @pytest.fixture(autouse=True)
-def mock_suds(mocker):
+def mock_suds_client(mocker):
     url = 'https://test.docdatapayments.com/ps/services/paymentservice/1_3?wsdl'
 
     # create a custom suds client with a wsdl and xsd saved on disk so we don't really connect
@@ -55,3 +43,10 @@ def mock_suds(mocker):
 
     # patch the CACHED_CLIENT so get_suds_client will return ours
     mocker.patch.dict("oscar_docdata.gateway.CACHED_CLIENT", {url: client})
+
+    return client
+
+
+@pytest.fixture()
+def mock_transport(mock_suds_client):
+    return mock_suds_client.options.transport
